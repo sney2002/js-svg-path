f460048b1ab182268c82a2f1d2f1026952edcf14
Jhonathan Salguero Villa <sney2002@gmail.com>
2019-08-21 08:20:17 -0500

    test build 2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import PointCurveShape from "./point-curve-shape.js";
 import Outline from "./outline.js";
 import SVGParser from "./svg-parser.js";
-
+console.log("HELLO WORLD sney2002");
 const API = { SVGParser, Outline, PointCurveShape, parse: function(pathString) {
   	const outline = new API.Outline();
     const parser = new API.SVGParser(outline);
